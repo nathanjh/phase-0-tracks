774251f606e8ccf5d11408cde887bd9c601811e5
Nathan Herrera <nathanherrera@gmail.com>
2016-08-03 11:11:59 -0700

    Add internal naming system for hamsters w/o nametags
    -Create new list file for internally named hamsters to read/write

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -5,7 +5,6 @@ def error_check(input_check)
 		input_check = gets.chomp
 	end
 end
-
 # Hamster Name
 puts "Does this hamster have a name? (y/n)\n"
 name = gets.chomp
@@ -15,7 +14,17 @@ if name == "y"
 	puts "What's this hamster's name?\n"
 	name = gets.chomp
 else
-	name = "Johnny"
+	##Internal naming system based on number of un-named hamsters
+	#combined with the name "Johhny".  Reads and writes from an external
+	#file (along with date/time of entry, and parses as an array to determine
+	#current "Johhny" number.
+	require 'date'
+	johnnys = File.open("johnnys.txt", "r") { |ham_names| 
+		ham_names.readlines
+	}
+	name = "Johnny".concat(johnnys.length.to_s)
+	puts name
+	File.open("johnnys.txt", "a") { |newname| newname.puts name + " acquired on: " +Time.now.strftime("%d/%m/%Y %H:%M")}
 end
 
 # Hamster Loudness
