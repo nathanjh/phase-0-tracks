fdff3186c8f4f200714f7dc612128c3ffcf57e85
Nathan Herrera <nathanherrera@gmail.com>
2016-08-04 23:31:40 -0700

    Build hash with interview answers, add logic test, wrap with a while loop.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,5 @@
+##add functionality to create and write to a list of all interviewees!!!!
+
 #age test helper method
 def age_test(age, birth_year)
 	require 'date'
@@ -9,26 +11,66 @@ def age_test(age, birth_year)
 	end
 end
 
-#name question
-puts "What is your name?\n"	
-name = gets.chomp 
+#list of known vampire aliases
+$vamp_names = ["Drake Cula", "Tu Fang", "O R Lok", "Angel"]
+#vampire test helper method
+def vamp_test(prof)
+	results = "Results inconclusive."
+	if $vamp_names.include?(prof[:name]) || prof[:allergies].include?("sunshine")
+		results = "Definitely a vampire."
+	elsif prof[:is_age] == false && prof[:likes_garlic] =="n" && prof[:wants_insurance] == "n"
+		results = "Almost certainly a vampire."
+	elsif prof[:is_age] == false && (prof[:likes_garlic] == "n" || prof[:wants_insurance] == "n")
+		results = "Probably a vampire."	
+	elsif prof[:is_age] == true && (prof[:likes_garlic] == "y" || prof[:wants_insurance] == "y")
+		results = "Probably not a vampire."
+	else
+		results
+	end
+end			
 
-#age questions
-puts "How old are you?\n"
-age = gets.chomp.to_i
-puts "What year were you born?\n"
-birth_year = gets.chomp.to_i
+#determine number of candidates to process
+puts "Please input number of interviews to be processed."
+times = gets.chomp.to_i
+i = 0
+while i < times
+	#collect profile data in a hash
+	prof = Hash.new 
 
-#garlic question
-puts "Our company cafeteria serves garlic bread.\nShould we order some for you?(Y/N)\n"
-likes_garlic = gets.chomp.downcase
+	#name question
+	puts "What is your name?\n"	
+	prof[:name] = gets.chomp 
 
-#insurance question
-puts "Would you like to enroll in the company's health insurance?(Y/N)\n"
-wants_insurance = gets.chomp.downcase
+	#age questions
+	puts "How old are you?\n"
+	prof[:age] = gets.chomp.to_i
+	puts "What year were you born?\n"
+	prof[:birth_year] = gets.chomp.to_i
+	prof[:is_age] = age_test(prof[:age], prof[:birth_year])
 
-prefs = [name, age, birth_year, likes_garlic, wants_insurance]
+	#garlic question
+	puts "Our company cafeteria serves garlic bread.\nShould we order some for you?(Y/N)\n"
+	prof[:likes_garlic] = gets.chomp.downcase
 
+	#insurance question
+	puts "Would you like to enroll in the company's health insurance?(Y/N)\n"
+	prof[:wants_insurance] = gets.chomp.downcase
 
-test_results = age_test(age, birth_year)
-puts test_results
+	#allergy question
+	prof[:allergies] = Array.new
+	puts "Please list any known allergies.\nPlease type 'done' or leave blank when finished."
+	allergy = gets.chomp
+	until allergy == "" || allergy == "done"
+		prof[:allergies] << allergy
+		if allergy == "sunshine"
+			break
+		end
+		allergy = gets.chomp
+	end
+
+	puts prof 
+	outcome = vamp_test(prof)
+	puts outcome
+	i += 1
+end
+
