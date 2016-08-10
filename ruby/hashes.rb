57dbf47f0182e8d36ab5d1b8fe60006f2ebd97f8
Nathan Herrera <nathanherrera@gmail.com>
2016-08-10 14:28:16 -0700

    Add user interface
    -Add questions, and store in hash keys/values
    -Process value update request (with correct data type)
    -Print results
    -Add test and additional comments to provide clarity

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -9,6 +9,7 @@
 #   -Get client trap doors/hidden room preference, store as boolean
 # -Print back hash keys/values for review
 # -Provide opportunity to update value for a given key 
+# (How do we know how to convert new modified input value to appropriate key-specific data type? see helper methods...)
 #   -Puts statement to prompt user, new variable for gets.chomp.to_sym to store input
 #   -If "none" or " ", reprint hash and exit
 #   -Else set new value
@@ -63,8 +64,48 @@ end
 #helper method to format key printing
 #   -iterates over the hash, and prints a nicely formatted string listing keys, values.
 def print_profile(hash)
-	puts "Client profile\n_______________"
+	puts "Client profile for #{hash[:name]}\n___________________"
 	hash.each { |k, v| puts "#{k}: #{v}"}
+end
+
+#create a new hash to store our client data
+
+client = {}
+# UI
+puts "Hooray, a new client!"
+#name
+puts "Please enter your name:"
+client[:name] = gets.chomp
+#age
+puts "Please enter your age:"
+client[:age] = gets.chomp.to_i
+#number of kids
+puts "Please enter number of children currently living in space to be remodeled:\n(please enter 0 if none)"
+client[:num_kids] = gets.chomp.to_i
+#decor style
+puts "Please give us a sense of your desired style of decor in one or two words:"
+client[:style] = gets.chomp
+#glass block lover?
+puts "How would you like a stunning new wall of glass blocks? (y/n)"
+client[:glass_blocks] = to_bool(gets.chomp)
+#home security question
+puts "Do you expect to require any trap doors or fake-bookcase-hidden-rooms? (y/n)"
+client[:trap_doors] = to_bool(gets.chomp)
+puts ".\n.\n.\n.\n.\n.\n.\n.\n.\nThanks for your input!!!\nPlease take a moment to review your answers..."
+print_profile(client)
+puts "Is there anything you'd like to change?\nIf so, please specify, otherwise leave blank or type 'none'."
+choice = gets.chomp.to_sym
+if choice == :none || choice == :""
+	puts "Thanks for your interest! We'll be in touch shortly!"
+	print_profile(client)
+else
+	puts "Please enter a new value for #{choice.to_s}"
+	#using helper methods, our program now knows what sort of data to store when given a valid key name!
+	client[choice] = convert_str_to(gets.chomp, data_type(choice, client))
+	#to test:
+	#puts "#{client[choice]} is an instance of #{client[choice].class}."
+	puts "Thanks for your interest! We'll be in touch shortly!"
+	print_profile(client)
 end
 
 
