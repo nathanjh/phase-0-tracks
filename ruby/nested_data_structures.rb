0c689fae723edc45e23c09dc1dbc7cdef561df74
Nathan Herrera <nathanherrera@gmail.com>
2016-08-12 15:00:02 -0700

    Inital commit
    -Add basic nested structure
    -Start populating data: entered 400IM times for mens/womens 2012 olympics

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,37 @@
+# Nested data structure consisting largely of hashes within hashes to represent
+#summer Olympics compteition results (selected).
+
+summer_olympics = {
+	2012 => {
+		swimming: {
+			mens_400m_im: {
+				"Ryan Lochte" => "4:05:18",
+				"Tiago Pereira" => "4:08:86",
+				"Kosuke Hagino" => "4:08:94",
+				"Michael Phelps" => "4:09:28",
+				"Chad le Clos" => "4:12:42",
+				"Yuya Horihata" => "4:13:49",
+				"Thomas Fraser-Holmes" => "4:13:49"
+				"Luca Marin" => "4:14:89"
+			}
+			womens_400m_im: {
+				"Ye Shiwen" => "4:28:43",
+				"Elizabeth Beisel" => "4:31:27"
+				"Li Xuanxu" => "4:32:91"
+				"Katinka Hosszu" => "4:33:49"
+				"Hannah Miley" => "4:34:17"
+				"Stephanie Rice" => "4:35:49"
+				"Caitlin Leverenz" => "4:35:49"
+				"Mereia Belmonte Garcia" => "4:35:62"
+			}
+		}
+		gymnastics: {
+			womens_all_around:
+			mens_all_around:
+		}
+
+	}
+	2016 => {
+
+	}
+}
