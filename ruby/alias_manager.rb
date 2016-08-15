96da299ae2e87f236adc7cd5e1385b3c16c77439
Nathan Herrera <nathanherrera@gmail.com>
2016-08-15 02:05:11 -0700

    Initial commit
    -start to add pseudocode for alias method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+#alias method pseudocode:
+#-build up new string of shifted letters by mapping new array of shifted letters, and join into a string
+#-conditionally shift letters:
+#   -test if letter is a 'letter' character or not
+#   	-if not, leave as is in new aliased string
+#   	-if so, test whether or not character is a vowel
+#          -if so, shift one to the right on list of vowels
+#		   -if not, shift one to the right on list of consonants
+##-reverse first and last names: 
+#   -convert string to array by splitting on the space
+#   -reverse name positions in array (new 'reversed' array)
+#   -join the new array back into a string 
