35fab8918581ca201d12d9e9550d4c5ff32f16a5
Nathan Herrera <nathanherrera@gmail.com>
2016-08-15 23:05:42 -0700

    Add array to store Santa objects
    -Add two-dimensional loop to initialize a different Santa instance for each possible gender identification per ethnicity option (96 in all)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,10 @@
 #define Santa class
 class Santa 
-	def initialize
-		p "Initializing Santa instance..."
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak 
@@ -18,6 +21,24 @@ end
 
 
 
-multiple_santa = Santa.new
-multiple_santa.speak
-multiple_santa.eat_milk_and_cookies("lusikkaleivat")
+# multiple_santa = Santa.new
+# multiple_santa.speak
+# multiple_santa.eat_milk_and_cookies("lusikkaleivat")
+
+
+#array to store santa objects
+santas = []
+
+gender_options = ["agender", "androgyne", "androgynous", "bigender", "cis", "cisgender", "cis female", "cis male", "FTM", 
+"gender fluid", "gender nonconforming", "genderqueer", "intersex", "male", "female", "N/A"]
+ethnicity_options = ["white", "black", "A. Indian/ Alaska nat.", "Asian", "Latino", "Hawaiian n."]
+
+#here's a bunch of santas...96 in all...
+ethnicity_options.length.times do |x| #outer loop for each possible ethnicity
+	gender_options.length.times do |y| #inner loop for each possible gender id
+		santas << Santa.new(gender_options[y], ethnicity_options[x])
+	end
+end
+
+
+
