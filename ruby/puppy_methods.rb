560b408bae065d9a45e1490e8a3e13d32603a800
Nathan Herrera <nathanherrera@gmail.com>
2016-08-16 19:33:29 -0700

    Add dragon class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,33 +1,61 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
 
-  def speak(int)
-  	int.times { puts "woof" }
-  end
 
-  def roll_over
-  	puts "*rolls over*"
-  end
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
 
-  def dog_years(years)
-  	dog_years = years * 7
-  end
+	def speak(int)
+		int.times { puts "woof" }
+	end
 
-  def shake
-  	puts "*extends paw*"
-  end
+	def roll_over
+		puts "*rolls over*"
+	end
+
+	def dog_years(years)
+		dog_years = years * 7
+	end
+
+	def shake
+		puts "*extends paw*"
+	end
 
 end
 
 
+class Dragons
 
-fido = Puppy.new
-fido.fetch("frisbee")
-fido.speak(4)
-fido.roll_over
-p fido.dog_years(7)
-fido.shake
+	def initialize
+		puts "Here comes a dragon..."
+	end
+
+	def breathes_fire
+		puts "Breathes fire.  A lot."
+	end
+
+	def fly
+		puts "Takes to the sky!"
+	end
+end
+
+
+
+
+
+
+
+
+
+# fido = Puppy.new
+# fido.fetch("frisbee")
+# fido.speak(4)
+# fido.roll_over
+# p fido.dog_years(7)
+# fido.shake
+
