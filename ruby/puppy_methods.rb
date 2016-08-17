4089f8f3c5aa71a5f99b02784bd6da4ee9202285
Nathan Herrera <nathanherrera@gmail.com>
2016-08-16 19:19:35 -0700

    Initial commit
    -Add instance methods and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  	int.times { puts "woof" }
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(years)
+  	dog_years = years * 7
+  end
+
+  def shake
+  	puts "*extends paw*"
+  end
+
 end
 
+
+
+fido = Puppy.new
+fido.fetch("frisbee")
+fido.speak(4)
+fido.roll_over
+p fido.dog_years(7)
+fido.shake
