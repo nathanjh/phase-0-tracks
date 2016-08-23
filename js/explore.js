01a1ea6768e73a500cc72e300626399372a24492
Nathan Herrera <nathanherrera@gmail.com>
2016-08-23 11:37:56 -0700

    Initial commit
    -Add pseodocode for reverse string function.

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+//Function to return reversed string
+//	-The input for this function will be a string
+//  -Declare an emplty string variable to hold our reversed string as it is built
+//  -Declare a counter variable to count backwards from the last character in input string
+//  	-Set counter variable equal to last character index of input string 
+//			-This will be the length of input string minus one
+//	-For each character in the string
+//		-Add character to reversed string variable
+//			-Character at input string index is equal to counter
+//		-Decrement counter by one while counter is greater than or equal to zero.
+//	-Output reversed string variable
+
