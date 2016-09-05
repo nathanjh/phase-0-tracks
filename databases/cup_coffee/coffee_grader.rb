61ef2fe0e69ea2063e36580fb865cf092d202b6b
Nathan Herrera <nathanherrera@gmail.com>
2016-09-04 23:23:32 -0700

    Initial commit, add some pseudocode

---
+++ databases/cup_coffee/coffee_grader.rb
@@ -0,0 +1,19 @@
+#create and maintain database of sensory scores for sample batches of coffee
+
+#should be easy to input scores, sample batch info, coffee info, and user profiles
+
+#should be able to produce meaningful reports about coffee evaluations by retrieving
+#stored data by:
+#averaging scores
+#selecting based on a discreet sample batch, a coffee, a user, a time period
+
+#will be comprised of: 
+#   a ui where user can:
+#      -create user profile
+#      -input batch information
+#      -add new coffees  
+#      -input sensory scores 
+#      -generate reports
+#   a module to be required that will contain all helper methods
+#   a database with four tables (users, coffees, batches/samples, scores)
+
