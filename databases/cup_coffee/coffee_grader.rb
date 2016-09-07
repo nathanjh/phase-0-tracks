8c472fdfd23bca26bfb29b63d0c696ffabad126a
Nathan Herrera <nathanherrera@gmail.com>
2016-09-06 18:45:42 -0700

    Add work on add sample functionality

--- databases/cup_coffee/coffee_grader.rb
+++ databases/cup_coffee/coffee_grader.rb
@@ -76,12 +76,71 @@ else
 	puts "Nice to see you again, #{user}!"
 end
 
-#print menu to screen, get selection
-puts "MAIN MENU".center(50, '-')
+running = true
 
-#   -menu has 1.score coffee(s), 2. new sample/batch, 3. new coffee, 4. reports 5. exit
-#case statement to handle user input (use to_i method return to test input validity)
-#
+while running
+	#print menu to screen, get selection
+	#   -menu has 1.score coffee(s), 2. new sample/batch, 3. new coffee, 4. reports 5. exit
+	puts "MAIN MENU".center(50, '-'), " [1] Score Coffee".ljust(30) << "[2] Add Sample/Roast"
+	puts " [3] Add Coffee".ljust(30) << "[4] Reports", " [5] Exit"
+	puts '-' * 50
+	puts "please select a number:"
+
+	user_choice = gets.chomp.to_i
+	while user_choice == "" || user_choice == 0 || user_choice > 5
+		puts "please enter a valid selection based on the above menu numbers:"
+		user_choice = gets.chomp.to_i	
+	end
+
+	#case statement to handle user input (use to_i method return to test input validity)
+	case user_choice
+	when 1
+	when 2
+		adding_sample = true
+		while adding_sample
+			puts "Let's add a sample for cupping.\n""Please enter a roast date in the format YYYY-MM-DD:"
+			roast_date = gets.chomp
+			until roast_date.length == 10
+				puts "Please use the format YYYY-MM-DD"
+				roast_date = gets.chomp 
+			end
+			puts "A great day! Please enter coffee name (capitalization matters!):"
+			coffee_name = gets.chomp
+			while !CoffeeUtilities.in_db?(db, 'coffees', 'name', coffee_name)
+				puts "I'm not familiar with that coffee.", "Would you like to add it?"
+				add_choice = CoffeeUi.answer_check('y', 'n')
+				if add_choice == 'y'
+					puts "Please enter an origin:"
+					coffee_origin = gets.chomp
+					CoffeeUtilities.create_coffee(db, coffee_name, coffee_origin)
+					puts "Added #{coffee_name}, #{coffee_origin} and..."
+				else
+					puts "Try again?"
+					try_again = CoffeeUi.answer_check('y', 'n', ' to try again', ' for main menu')
+					if try_again == 'n'
+						adding_sample = false
+						break #TODO fix this so that we exit to main menu!
+					else
+						puts "Please enter coffee name:"
+						coffee_name = gets.chomp
+					end
+				end
+			end 
+			CoffeeUtilities.new_sample(db, CoffeeUtilities.get_id_from_name(db, 'coffees', coffee_name), roast_date)
+			puts "Added new sample of #{coffee_name}, roasted on #{roast_date}.\n"
+			puts "Again?"
+			sample_again = CoffeeUi.answer_check('y', 'n', ' to add another sample', ' for main menu')
+			if sample_again == 'n'
+				adding_sample = false
+			end
+		end
+	when 3
+	when 4
+	else
+		puts "Thanks for using...goodbye!" 
+		running = false
+	end
+end
 
 
 
--- databases/cup_coffee/coffee_ui.rb
+++ databases/cup_coffee/coffee_ui.rb
@@ -2,17 +2,17 @@ module CoffeeUi
 	#UI helper methods
 	module_function
 
-	#to process user name input
-	#input is a string, test-method(predicate method), add-method
-	#   call predicate method with string and relevant db info as arguments (in 
-	#      this case, to test whether name exists in users.db)
-	#   if method returs false, call add method (for our purposes, to add string as 
-	#      a name to users.db )
-	#   return name-string
-	def user_name(name, pred_method, method)
-		if pred_method == false 
-			puts "name not found, adding...."
-			method 
-		end
-	end	
+	#add helper method to refactor user response checking
+	#clunky optional text messages need spaces in front to print correctly!
+
+	def answer_check(ans1, ans2, op_text1="", op_text2="")
+			variable = gets.chomp
+			until variable == ans1 || variable == ans2
+				puts "Please enter #{ans1}#{op_text1} or #{ans2}#{op_text2}"
+				variable = gets.chomp
+			end
+			variable
+	end
+
+
 end
