4e9b1a635cd32c75c7624dd0f70a61a18d842262
Nathan Herrera <nathanherrera@gmail.com>
2016-09-12 02:49:06 -0700

    Add GET routes as per 9.4.0

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,42 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+#write a GET route that displays an address 
+get '/contact' do 
+  "<a href='http://www.opry.com'>2804 Opryland Dr, Nashville, TN 37214</a>"
+end
+
+#write a GET route that takes a name as a query parameter 
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+#write a GET route that adds two numbers (as route parameters)
+get '/:num_1/plus/:num_2' do 
+  x = params[:num_1].to_f
+  y = params[:num_2].to_f
+  z = x + y 
+  ans = z.to_s
+  "#{params[:num_1].to_f.to_s} plus #{params[:num_2].to_f.to_s} equals #{ans}!"
+end
+
+#write a GET route that filters students by campus
+get '/campus/:campus' do
+  by_campus = db.execute("SELECT name FROM students WHERE campus=?", [params[:campus]])
+  response = "students attending #{params[:campus]} campus:<br>"
+  by_campus.each do |student|
+    response << "#{student['name']}<br>"
+  end
+  response
+end
+
+
+
+
+
